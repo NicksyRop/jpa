5d3ea37f54a280a46f6b15b1372197baca291985
NicksyRop <nicksonkipkorir25@gmail.com>
2022-12-08 13:19:18 +0300

    created Customers entity

--- src/main/java/com/restservice/jpa/Customer.java
+++ src/main/java/com/restservice/jpa/Customer.java
@@ -1,6 +1,35 @@
 package com.restservice.jpa;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Long id;
+    private String firstname;
+    private String lastname;
+
+
+    public Customer(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
 }
